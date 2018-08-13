0e505d5175245a918102b105bb14be3427eb1741
livelybone <2631541504@qq.com>
2018-08-13 11:06:45 +0800

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.copyDom = require('./lib/copy-dom');
+exports.copyText = require('./lib/copy-text');
+exports.simpleCopy = require('./lib/object-simple-copy');
+exports.deepCopy = require('./lib/object-deep-copy');
