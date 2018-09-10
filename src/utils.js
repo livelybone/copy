42fd14f3894c7c796f3affeba57e02a846200de3
livelybone <2631541504@qq.com>
2018-09-10 17:23:52 +0800

    update function expectedObjType

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,14 @@
 /**
  * @description If param obj is expected that can be copied
  * */
-export var expectedObjType = (function (obj) {
-  var keys = ['Date', 'Error', 'RegExp', 'FileList', 'File', 'Element']
+export function expectedObjType(obj) {
   return typeof obj === 'object'
     && !(obj === null
-      || keys.some(function (k) {
-        return typeof this[k] === 'function' && (obj instanceof this[k])
-      }))
-}).bind(this)
+      || obj instanceof Date
+      || obj instanceof Error
+      || obj instanceof RegExp)
+    && (typeof window === 'undefined'
+      || !(obj instanceof FileList
+        || obj instanceof File
+        || obj instanceof Element))
+}
